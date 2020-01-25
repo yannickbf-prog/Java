344e552faf1bec63bfedc1fc986f0a4331e00b1f
yannickbf-prog <ybaratte@cifpfbmoll.eu>
2020-01-25 22:04:23 +0100

    Hago commit para subir los archivos de la practica, en este punto no se como seguir para hacer la transferencia entre 2 personas

---
+++ Practica3.1Cuenta/src/practica3/pkg1cuenta/Cuenta.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3.pkg1cuenta;
+
+/**
+ *
+ * @author Yann
+ */
+public class Cuenta {
+    private String nombreCliente;
+    private String numeroCuenta;
+    private double tipoIneteres;
+    private double saldo;
+    
+    public Cuenta(){
+        
+    }
+
+    public Cuenta(String nombreCliente, String numeroCuenta, double tipoIneteres, double saldo) {
+        this.nombreCliente = nombreCliente;
+        this.numeroCuenta = numeroCuenta;
+        this.tipoIneteres = tipoIneteres;
+        this.saldo = saldo;
+    }
+    
+    public Cuenta(Cuenta C1) {
+        this.nombreCliente = C1.nombreCliente;
+        this.numeroCuenta = C1.numeroCuenta;
+        this.tipoIneteres = C1.tipoIneteres;
+        this.saldo = C1.saldo;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public void setTipoIneteres(double tipoIneteres) {
+        this.tipoIneteres = tipoIneteres;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public double getTipoIneteres() {
+        return tipoIneteres;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+    
+    public boolean ingreso(double cantidad){
+        double saldoActual=this.getSaldo();
+        this.setSaldo(saldoActual+cantidad);
+        return getSaldo()==saldoActual+cantidad;
+             
+    }
+    
+    public boolean reintegro(double cantidad){
+        double saldoActual=this.getSaldo();
+        if(saldoActual>cantidad && cantidad>0){
+            this.setSaldo(saldoActual-cantidad);
+        }
+        else{
+            System.out.println("No puedes retirar " + cantidad + " euros por que supera la cantidad que tienes en tu cuenta o no has puesto una cifra valida");
+        }
+        return getSaldo()==saldoActual-cantidad;
+    }
+    
+    public void transferencia(String cuentaDestino, double importe){
+        this.setSaldo(this.getSaldo()-importe);
+        
+    }
+
+}
+
+
---
+++ Practica3.1Cuenta/src/practica3/pkg1cuenta/Practica31Cuenta.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3.pkg1cuenta;
+
+/**
+ *
+ * @author Yann
+ */
+public class Practica31Cuenta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cuenta Yannick=new Cuenta("Yannick", "0101", 4.3, 1000.35);
+        
+        Cuenta Juanito=new Cuenta("Juanito", "1010", 4.5, 500.00);
+        
+        Yannick.ingreso(50);
+        
+        System.out.println(Yannick.getSaldo());
+        
+        Yannick.reintegro(10);
+        
+        System.out.println(Yannick.getSaldo());
+    }
+    
+}
